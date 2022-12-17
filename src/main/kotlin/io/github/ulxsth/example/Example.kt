95a64e1b468bae8949266c2750e87db34bd7a160
ulxsth <ulxsth@gmail.com>
2022-12-17 23:31:03 +0900

    リスナを登録

--- src/main/kotlin/io/github/ulxsth/example/Example.kt
+++ src/main/kotlin/io/github/ulxsth/example/Example.kt
@@ -1,13 +1,10 @@
 package io.github.ulxsth.example
 
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.event.Listener
 
-class Example : JavaPlugin() {
+class Example : JavaPlugin(), Listener {
     override fun onEnable() {
-        
-    }
-
-    override fun onDisable() {
-
+        this.getServer().getPluginManager().registerEvents(this, this)
     }
 }
