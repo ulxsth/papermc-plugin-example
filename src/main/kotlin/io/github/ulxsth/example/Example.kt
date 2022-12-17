cc4ddab4711bac8bc9f23ef870c3b6d2872cb294
ulxsth <ulxsth@gmail.com>
2022-12-17 17:06:13 +0900

    init

---
+++ src/main/kotlin/io/github/ulxsth/example/Example.kt
@@ -0,0 +1,13 @@
+package io.github.ulxsth.example
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Example : JavaPlugin() {
+    override fun onEnable() {
+        
+    }
+
+    override fun onDisable() {
+
+    }
+}
